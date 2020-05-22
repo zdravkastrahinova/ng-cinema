c90bbab26d633b69cf37c8ffd2d569174fd11bd9
Zdravka Strahinova <zdravkastrahinova1022@gmail.com>
2020-05-22 17:00:05 +0300

    Implement login

---
+++ cinema-app/src/app/auth/login/login.component.ts
@@ -0,0 +1,63 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService} from '../services/authentication.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+  form: FormGroup;
+  errorMessage: string;
+
+  destroy$ = new Subject<boolean>();
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private authService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  onSubmit(): void {
+    this.errorMessage = null;
+
+    const email = this.form.controls.email.value;
+    const password = this.form.get('password').value;
+
+    this.authService.login(email, password).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(response => {
+      if (!response) {
+        this.errorMessage = 'Invalid email or password.';
+
+        return;
+      }
+
+      // store logged user
+      this.authService.setLoggedUser(response);
+
+      // redirect to main app
+      this.router.navigate(['movies-card-list']);
+    });
+  }
+
+  private buildForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+}
